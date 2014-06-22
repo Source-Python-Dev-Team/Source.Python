0044ef825ee88b8cf1ea082279826f42bf0a0537
Stephen Toon <satoon101@gmail.com>
2014-06-22 18:39:46 -0400

    Updated Build.sh to work when the repositories path includes spaces

--- src/Build.sh
+++ src/Build.sh
@@ -1,5 +1,5 @@
 # Store the start directory for later reference
-STARTDIR=$PWD
+STARTDIR="$PWD"
 
 
 # A function to choose the branch to build against
@@ -11,8 +11,11 @@ ChooseBranch () {
     # Store a base counting variable
     num=0
 
+    # Navigate to the makefiles/branch directory
+    cd makefiles/branch
+
     # Loop through all branches supported by the plugin
-    for filepath in $(find $STARTDIR/makefiles/branch -maxdepth 1 -type f)
+    for filepath in $(find . -maxdepth 1 -type f -print0 | xargs -0)
     do
 
         # Increment the counter
@@ -85,15 +88,15 @@ ChooseBuildType () {
 CloneRepo () {
 
     # Set the branch's repository directory
-    BRANCHDIR=$STARTDIR/hl2sdk/$BRANCH
+    BRANCHDIR="$STARTDIR/hl2sdk/$BRANCH"
 
     # If the hl2sdk directory does not exist, create the clone
-    if [ ! -d $BRANCHDIR ]; then
-        mkdir -p $BRANCHDIR
-        cd $BRANCHDIR
+    if [ ! -d "$BRANCHDIR" ]; then
+        mkdir -p "$BRANCHDIR"
+        cd "$BRANCHDIR"
         git clone https://github.com/alliedmodders/hl2sdk.git .
     else
-        cd $BRANCHDIR
+        cd "$BRANCHDIR"
     fi
 
     # Try to checkout the selected branch and revert all changes
@@ -127,12 +130,12 @@ PullChanges () {
 MovePatches () {
 
     # Set the branch's patch directory
-    PATCHDIR=$STARTDIR/patches/$BRANCH
+    PATCHDIR="$STARTDIR/patches/$BRANCH"
 
     # Copy any patched files over if any exist for the specific branch
-    if [ -d $PATCHDIR ]; then
-        cd $PATCHDIR
-        cp -r * $BRANCHDIR
+    if [ -d "$PATCHDIR" ]; then
+        cd "$PATCHDIR"
+        cp -r * "$BRANCHDIR"
     fi
 
     # Create the build files for the branch
@@ -144,28 +147,28 @@ MovePatches () {
 CreateBuild () {
 
     # Navigate back to the starting directory
-    cd $STARTDIR
+    cd "$STARTDIR"
 
     # Set the branch's build directory
-    BUILDDIR=$STARTDIR/Builds/$BRANCH
+    BUILDDIR="$STARTDIR/Builds/$BRANCH"
 
     # Does the build directory exist (make it if not)?
-    if [ ! -d $BUILDDIR ]; then
-        mkdir -p $BUILDDIR
+    if [ ! -d "$BUILDDIR" ]; then
+        mkdir -p "$BUILDDIR"
     fi
 
     # Building for "Release"?
     if [ $BUILD_TYPE == "1" ]; then
-        cmake . -B$BUILDDIR -DBRANCH=$BRANCH
+        cmake . -B"$BUILDDIR" -DBRANCH=$BRANCH
 
     # Building for "Debug"?
     else
-        cmake . -B$BUILDDIR -DBRANCH=$BRANCH -DCMAKE_BUILD_TYPE=Debug
+        cmake . -B"$BUILDDIR" -DBRANCH=$BRANCH -DCMAKE_BUILD_TYPE=Debug
 
     fi
 
     # Navigate to the ../Builds/<Branch> directory
-    cd $BUILDDIR
+    cd "$BUILDDIR"
 
     # Build the binaries
     make clean
