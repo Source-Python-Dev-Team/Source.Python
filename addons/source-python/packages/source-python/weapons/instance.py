ea05210b88d5bdd5bc3f85ac6bdc32c76c23b622
Stephen Toon <satoon101@gmail.com>
2014-11-17 13:00:05 -0500

    Moved weapons.manager._Weapon to new weapons.instance module and made the class public.
    Made listeners.tick.delays._Delay public and added a 'cancel' method to more easily cancel an instance.
    
    Removed '__all__' declaration from listeners.tick.delays and listeners.tick.repeat as the objects are exposed via listeners.tick.__init__

---
+++ addons/source-python/packages/source-python/weapons/instance.py
@@ -0,0 +1,90 @@
+# ../weapons/instance.py
+
+"""Provides a class to store information specific to each weapon."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Cvars
+from cvars import ConVar
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = ('Weapon',
+           )
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class Weapon(object):
+
+    """Class used to store information specific to the given weapon."""
+
+    def __init__(self, name, basename, properties):
+        """Store the base attributes for the weapon."""
+        # Store the weapon's full name
+        self._name = name
+
+        # Store the weapon's base name
+        self._basename = basename
+
+        # Store the weapon's slot number
+        self._slot = properties.get('slot', None)
+
+        # Store the weapon's max ammo amount
+        self._maxammo = properties.get('maxammo', None)
+
+        # Store the weapon's ammo property
+        self._ammoprop = properties.get('ammoprop', None)
+
+        # Store the weapon's clip amount
+        self._clip = properties.get('clip', 0)
+
+        # Store the weapon's tags
+        self._tags = properties.get('tags', 'all').split(',')
+
+    @property
+    def name(self):
+        """Return the classname of the weapon."""
+        return self._name
+
+    @property
+    def basename(self):
+        """Return the basename of the weapon."""
+        return self._basename
+
+    @property
+    def slot(self):
+        """Return the slot of the weapon."""
+        return self._slot
+
+    @property
+    def maxammo(self):
+        """Return the maxammo amount for the weapon."""
+        # Is the stored maxammo an integer?
+        if isinstance(self._maxammo, int):
+
+            # Return the value
+            return self._maxammo
+
+        # Get the cvar value of the maxammo
+        return ConVar(self._maxammo).get_int()
+
+    @property
+    def ammoprop(self):
+        """Return the ammoprop of the weapon."""
+        return self._ammoprop
+
+    @property
+    def clip(self):
+        """Return the clip value of the weapon."""
+        return self._clip
+
+    @property
+    def tags(self):
+        """Return the tags of the weapon."""
+        return self._tags
