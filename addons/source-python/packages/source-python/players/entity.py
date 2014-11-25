0549a1624df36e8d09c39f09e44578c0a3f3f633
Stephen Toon <satoon101@gmail.com>
2014-11-25 12:33:43 -0500

    Added get_trace_ray, get_view_coordinates, get_view_entity, get_veiw_player, get_eye_location, and get_view_vector methods to PlayerEntity.

--- addons/source-python/packages/source-python/players/entity.py
+++ addons/source-python/packages/source-python/players/entity.py
@@ -5,11 +5,29 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Python Imports
+#   Math
+from math import cos
+from math import radians
+from math import sin
+
 # Source.Python Imports
+#   Constants
+from constants import MAX_TRACE_LENGTH
 #   Engines
 from engines.server import engine_server
+from engines.trace import engine_trace
+from engines.trace import ContentMasks
+from engines.trace import GameTrace
+from engines.trace import Ray
+from engines.trace import TraceFilterSimple
 #   Entities
 from entities.entity import BaseEntity
+from entities.helpers import index_from_pointer
+#   Mathlib
+from mathlib import Vector
+#   Memory
+from memory import Pointer
 #   Players
 from players.helpers import playerinfo_from_index
 from players.helpers import address_from_playerinfo
@@ -107,3 +125,75 @@ class PlayerEntity(BaseEntity, _PlayerWeapons):
     team = property(
         get_team, set_team,
         doc="""Property to get/set the player's team.""")
+
+    def get_trace_ray(self, mask=ContentMasks.ALL, trace_filter=None):
+        """Return the player's current trace data."""
+        # Get the eye location of the player
+        start_vec = self.get_eye_location()
+
+        # Calculate the greatest possible distance
+        end_vec = start_vec + self.get_view_vector() * MAX_TRACE_LENGTH
+
+        # Create a new trace object
+        trace = GameTrace()
+
+        # Start the trace
+        engine_trace.trace_ray(
+            Ray(start_vec, end_vec), mask, TraceFilterSimple(
+                (self.index,)) if trace_filter is None else trace_filter,
+            trace
+        )
+
+        # Return the trace data
+        return trace
+
+    def get_view_coordinates(self):
+        """Return the coordinates the player is currently looking at."""
+        # Get the player's current trace data
+        trace = self.get_trace_ray()
+
+        # Return the end position of the trace if it hit something
+        return trace.end_position if trace.did_hit() else None
+
+    def get_view_entity(self):
+        """Return the entity that the player is looking at."""
+        # Get the player's current trace data
+        trace = self.get_trace_ray()
+
+        # Did the trace hit?
+        if not trace.did_hit():
+            return None
+
+        # Get the entity that the trace hit
+        pointer = Pointer(trace.get_entity().get_base_entity())
+
+        # Get the index of the entity the trace hit
+        index = index_from_pointer(pointer, False)
+
+        # Return a BaseEntity instance of the hit entity if it has an index
+        return BaseEntity(index) if index else None
+
+    def get_view_player(self):
+        """Return the player that the player is looking at."""
+        # Get the entity that the player is looking at
+        entity = self.get_view_entity()
+
+        # Return a PlayerEntity instance of the player or None if not a player
+        return PlayerEntity(
+            entity.index) if entity.classname == 'player' else None
+
+    def get_eye_location(self):
+        """Return the eye location of the player."""
+        return Vector(*tuple(self.get_prop_float(
+            'm_vecViewOffset[{0}]'.format(x)) + y for x, y in
+            enumerate(self.origin)))
+
+    def get_view_vector(self):
+        """Return the view vector of the player."""
+        eye_angle0 = self.get_prop_float('m_angEyeAngles[0]')
+        eye_angle1 = self.get_prop_float('m_angEyeAngles[1]')
+        return Vector(
+            cos(radians(eye_angle1)),
+            sin(radians(eye_angle1)),
+            -1 * sin(radians(eye_angle0))
+        )
