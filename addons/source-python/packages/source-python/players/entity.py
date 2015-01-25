8d782415cf274a5581de2ef88d2c20d1a64e97fa
Stephen Toon <satoon101@gmail.com>
2015-01-25 10:50:18 -0500

    Updated credits.

--- addons/source-python/packages/source-python/players/entity.py
+++ addons/source-python/packages/source-python/players/entity.py
@@ -180,8 +180,9 @@ class PlayerEntity(BaseEntity, _GameWeapons, _PlayerWeapons):
         entity = self.get_view_entity()
 
         # Return a PlayerEntity instance of the player or None if not a player
-        return (PlayerEntity(entity.index)
-            if entity is not None and entity.classname == 'player' else None)
+        return (
+            PlayerEntity(entity.index) if entity is not None
+            and entity.classname == 'player' else None)
 
     def get_eye_location(self):
         """Return the eye location of the player."""
