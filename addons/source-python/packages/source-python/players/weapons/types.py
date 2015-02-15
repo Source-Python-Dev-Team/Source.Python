a52a3a5152c529609c7d8e15c3d2d0e5a90e0c9d
Stephen Toon <satoon101@gmail.com>
2015-02-15 13:56:41 -0500

    Updated players.weapons.types to use a more updated method to return has_c4.

--- addons/source-python/packages/source-python/players/weapons/types.py
+++ addons/source-python/packages/source-python/players/weapons/types.py
@@ -6,10 +6,8 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python Imports
-#   Entities
-from entities import EntityGenerator
-from entities.entity import BaseEntity
-from entities.helpers import index_from_edict
+#   Filters
+from filters.entities import EntityIter
 
 
 # =============================================================================
@@ -22,16 +20,10 @@ class _C4(object):
     def has_c4(self):
         """Return whether or not the player is carrying C4."""
         # Loop through all c4 entities on the server
-        for edict in EntityGenerator('weapon_c4'):
-
-            # Get the entity's index
-            index = index_from_edict(edict)
-
-            # Get the entity's BaseEntity instance
-            entity = BaseEntity(index)
+        for weapon in EntityIter('weapon_c4', return_types='entity'):
 
             # Is the entity's "owner" the player?
-            if entity.owner == self.handle.to_int():
+            if weapon.owner == self.handle.to_int():
 
                 # Return True
                 return True
