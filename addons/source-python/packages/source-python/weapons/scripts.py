79bb020ca8c561cb508c22f89d49ff1a7b4c6f9a
Ayuto <ayuto@web.de>
2015-03-09 21:01:27 +0100

    Updated get_weapon_info return type to directly use WeaponInfo
    Removed unused imports

--- addons/source-python/packages/source-python/weapons/scripts.py
+++ addons/source-python/packages/source-python/weapons/scripts.py
@@ -11,11 +11,9 @@ from core import SOURCE_ENGINE
 #   Paths
 from paths import SP_DATA_PATH
 #   Memory
-from memory import make_object
 from memory.manager import manager
 #   Weapons
 from weapons.constants import WeaponID
-from weapons.constants import WeaponSound
 
 
 # =============================================================================
@@ -39,19 +37,12 @@ __all__ = ('BaseWeaponInfo',
 # =============================================================================
 # >> GLOBAL VARIABLES
 # =============================================================================
-_pipe = manager.create_pipe_from_file(SP_DATA_PATH.joinpath('weapons',
-    'scripts', SOURCE_ENGINE + '.ini'))
+get_weapon_info = manager.create_pipe_from_file(SP_DATA_PATH.joinpath(
+    'weapons', 'scripts', SOURCE_ENGINE + '.ini')).get_weapon_info
 
 weapon_scripts = dict()
 for weapon_id in WeaponID.values:
-    weapon_info = make_object(WeaponInfo, _pipe.get_weapon_info(weapon_id))
+    weapon_info = get_weapon_info(weapon_id)
     if weapon_info is None:
         continue
     weapon_scripts[weapon_info.class_name] = weapon_info
-
-
-# =============================================================================
-# >> HELPER FUNCTIONS
-# =============================================================================
-def get_weapon_info(weapon_id):
-    return make_object(WeaponInfo, _pipe.get_weapon_info(weapon_id))
