8275d2198cf397e0951b055fb742a250627cb293
L'In20Cible <invincible.qc@hotmail.com>
2015-04-07 15:34:45 -0400

    - Exported modtype_t as ModelType (forwarded to studio.constants). - Exported studio flags constants wrapped as StudioFlags enumerator (forwarded to studio.constants).

---
+++ addons/source-python/packages/source-python/studio/constants.py
@@ -0,0 +1,59 @@
+# ../studio/constants.py
+
+"""Provides model based functionality."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Python Imports
+#   Enum
+from enum import IntEnum
+
+
+# =============================================================================
+# >> FORWARD IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Studio
+from _studio._constants import ModelType
+from _studio._constants import STUDIO_NONE
+from _studio._constants import STUDIO_RENDER
+from _studio._constants import STUDIO_VIEWXFORMATTACHMENTS
+from _studio._constants import STUDIO_DRAWTRANSLUCENTSUBMODELS
+from _studio._constants import STUDIO_TWOPASS
+from _studio._constants import STUDIO_STATIC_LIGHTING
+from _studio._constants import STUDIO_WIREFRAME
+from _studio._constants import STUDIO_ITEM_BLINK
+from _studio._constants import STUDIO_NOSHADOWS
+from _studio._constants import STUDIO_WIREFRAME_VCOLLIDE
+from _studio._constants import STUDIO_TRANSPARENCY
+from _studio._constants import STUDIO_SHADOWDEPTHTEXTURE
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = ('ModelType',
+           'StudioFlags',
+           )
+
+
+# =============================================================================
+# >> ENUMERATORS
+# =============================================================================
+class StudioFlags(IntEnum):
+
+    """Studio flags wrapper enumerator."""
+
+    NONE = STUDIO_NONE
+    RENDER = STUDIO_RENDER
+    VIEWXFORMATTACHMENTS = STUDIO_VIEWXFORMATTACHMENTS
+    DRAWTRANSLUCENTSUBMODELS = STUDIO_DRAWTRANSLUCENTSUBMODELS
+    TWOPASS = STUDIO_TWOPASS
+    STATIC_LIGHTING = STUDIO_STATIC_LIGHTING
+    WIREFRAME = STUDIO_WIREFRAME
+    ITEM_BLINK = STUDIO_ITEM_BLINK
+    NOSHADOWS = STUDIO_NOSHADOWS
+    WIREFRAME_VCOLLIDE = STUDIO_WIREFRAME_VCOLLIDE
+    TRANSPARENCY = STUDIO_TRANSPARENCY
+    SHADOWDEPTHTEXTURE = STUDIO_SHADOWDEPTHTEXTURE
---
+++ src/core/modules/studio/studio_constants_wrap_python.cpp
@@ -0,0 +1,83 @@
+/**
+* =============================================================================
+* Source Python
+* Copyright (C) 2014 Source Python Development Team.  All rights reserved.
+* =============================================================================
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, the Source Python Team gives you permission
+* to link the code of this program (as well as its derivative works) to
+* "Half-Life 2," the "Source Engine," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, the Source.Python
+* Development Team grants this exception to all derivative works.
+*/
+
+//-----------------------------------------------------------------------------
+// Includes.
+//-----------------------------------------------------------------------------
+#include "export_main.h"
+#include "model_types.h"
+
+
+//-----------------------------------------------------------------------------
+// Forward declarations.
+//-----------------------------------------------------------------------------
+void export_model_type(scope);
+void export_studio_flags(scope);
+
+
+//-----------------------------------------------------------------------------
+// Declare the _studio._constants module.
+//-----------------------------------------------------------------------------
+DECLARE_SP_SUBMODULE(_studio, _constants)
+{
+	export_model_type(_constants);
+	export_studio_flags(_constants);
+}
+
+
+//-----------------------------------------------------------------------------
+// Exports model types.
+//-----------------------------------------------------------------------------
+void export_model_type(scope _constants)
+{
+	enum_<modtype_t> ModelType("ModelType");
+
+	// Values...
+	ModelType.value("BAD", mod_bad);
+	ModelType.value("BRUSH", mod_brush);
+	ModelType.value("SPRITE", mod_sprite);
+	ModelType.value("STUDIO", mod_studio);
+}
+
+
+//-----------------------------------------------------------------------------
+// Exports studio flags.
+//-----------------------------------------------------------------------------
+void export_studio_flags(scope _constants)
+{
+	_constants.attr("STUDIO_NONE") = STUDIO_NONE;
+	_constants.attr("STUDIO_RENDER") = STUDIO_RENDER;
+	_constants.attr("STUDIO_VIEWXFORMATTACHMENTS") = STUDIO_VIEWXFORMATTACHMENTS;
+	_constants.attr("STUDIO_DRAWTRANSLUCENTSUBMODELS") = STUDIO_DRAWTRANSLUCENTSUBMODELS;
+	_constants.attr("STUDIO_TWOPASS") = STUDIO_TWOPASS;
+	_constants.attr("STUDIO_STATIC_LIGHTING") = STUDIO_STATIC_LIGHTING;
+	_constants.attr("STUDIO_WIREFRAME") = STUDIO_WIREFRAME;
+	_constants.attr("STUDIO_ITEM_BLINK") = STUDIO_ITEM_BLINK;
+	_constants.attr("STUDIO_NOSHADOWS") = STUDIO_NOSHADOWS;
+	_constants.attr("STUDIO_WIREFRAME_VCOLLIDE") = STUDIO_WIREFRAME_VCOLLIDE;
+	_constants.attr("STUDIO_TRANSPARENCY") = STUDIO_TRANSPARENCY;
+	_constants.attr("STUDIO_SHADOWDEPTHTEXTURE") = STUDIO_SHADOWDEPTHTEXTURE;
+}
