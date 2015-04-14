df9fa8a2c9b7c32b8af9edfb87f956c8dfa89219
Stephen Toon <satoon101@gmail.com>
2015-04-13 20:44:46 -0400

    Updated BaseEntity.damage to BaseEntity.hurt and provided many fixes.

--- addons/source-python/packages/source-python/entities/specials.py
+++ addons/source-python/packages/source-python/entities/specials.py
@@ -5,14 +5,21 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Python Imports
+#   Contextlib
+from contextlib import suppress
+
 # Source.Python Imports
 #   Core
 from core import GAME_NAME
 #   Entities
 from entities import TakeDamageInfo
+from entities.constants import DamageTypes
 from entities.helpers import index_from_inthandle
 #   Filters
 from filters.weapons import WeaponClassIter
+#   Players
+from players.constants import HitGroup
 
 
 # =============================================================================
@@ -30,75 +37,95 @@ class _EntitySpecials(object):
 
     """Base class used to hold special functionality."""
 
-    def damage(
-            self, damage=0, damage_type=0,
-            weapon_index=None, hitgroup=0, **kwargs):
-        """Method used to hurt another entity with the given arguments."""
+    def hurt(
+            self, damage, damage_type=DamageTypes.GENERIC, attacker_index=None,
+            weapon_index=None, hitgroup=HitGroup.GENERIC, **kwargs):
+        """Method used to hurt the entity with the given arguments."""
         # Import BaseEntity classes
         # Doing this in the global scope causes cross import errors
         from entities.entity import BaseEntity
-        from players.entity import PlayerEntity
+        from weapons.entity import WeaponEntity
 
         # Is the game supported?
         if not hasattr(self, 'take_damage'):
 
             # Raise an error if not supported
             raise NotImplementedError(
-                'damage is not implemented for {0}'.format(GAME_NAME))
+                '"take_damage" is not implemented for {0}'.format(GAME_NAME))
 
-        # Was no weapon index given?
-        if weapon_index is None and hasattr(self, 'active_weapon'):
+        # Store values for later use
+        attacker = None
+        weapon = None
 
-            # Get the player's active weapon
-            weapon_index = index_from_inthandle(self.active_weapon)
+        # Was an attacker given?
+        if attacker_index is not None:
 
-        # Is the weapon invalid?
-        if weapon_index is None:
+            # Try to get the BaseEntity instance of the attacker
+            with suppress(ValueError):
+                attacker = BaseEntity(attacker_index)
 
-            # Get a _FakeWeapon instance
-            weapon = _FakeWeapon()
+        # Was a weapon given?
+        if weapon_index is not None:
 
-        # Is the weapon valid?
-        else:
+            # Try to get the WeaponEntity instance of the weapon
+            with suppress(ValueError):
+                weapon = WeaponEntity(weapon_index)
 
-            # Get the weapon's BaseEntity instance
-            weapon = BaseEntity(weapon_index)
+        # Is there a weapon but no attacker?
+        if attacker is None and weapon is not None:
 
-        # Get the victim's BaseEntity instance.
-        victim = BaseEntity(self.index)
+            # Try to get the attacker based off of the weapon's owner
+            with suppress(ValueError):
+                attacker_index = index_from_inthandle(weapon.current_owner)
+                if attacker_index is not None:
+                    attacker = BaseEntity(attacker_index)
 
-        # Is the victim a player?
-        if victim.classname == 'player':
+        # Is there an attacker but no weapon?
+        if attacker is not None and weapon is None:
 
-            # Get the victim's PlayerEntity instance instead
-            victim = PlayerEntity(self.index)
+            # Does the attacker have a weapon attribute?
+            if hasattr(attacker, 'active_weapon'):
 
-            # Is hitgroup a valid attribute?
-            if hasattr(victim, 'hitgroup'):
+                # Get the attacker's current weapon index
+                weapon_index = index_from_inthandle(
+                    attacker.active_weapon, False)
 
-                # Set the victim's hitgroup
-                victim.hitgroup = hitgroup
+                # Get the weapon's WeaponEntity instance if it is valid
+                if weapon_index is not None:
+                    weapon = WeaponEntity(weapon_index)
+
+        # Is hitgroup a valid attribute?
+        if hasattr(self, 'hitgroup'):
+
+            # Set the hitgroup
+            self.hitgroup = hitgroup
 
         # Get a TakeDamageInfo instance
         take_damage_info = TakeDamageInfo()
 
-        # Is the weapon a projectile?
-        if weapon.classname in _projectile_weapons:
+        # Is there a valid weapon?
+        if weapon is not None:
 
-            # Set the inflictor to the weapon's index
-            take_damage_info.inflictor = weapon.index
+            # Is the weapon a projectile?
+            if weapon.classname in _projectile_weapons:
 
-        # Is the weapon not a projectile?
-        else:
+                # Set the inflictor to the weapon's index
+                take_damage_info.inflictor = weapon.index
 
-            # Set the inflictor to the entity's index
-            take_damage_info.inflictor = self.index
+            # Is the weapon not a projectile and the attacker is valid?
+            elif attacker_index is not None:
 
-        # Set the attacker to the entity's index
-        take_damage_info.attacker = self.index
+                # Set the inflictor to the attacker's index
+                take_damage_info.inflictor = attacker_index
 
-        # Set the weapon to the weapon's index
-        take_damage_info.weapon = weapon.index
+            # Set the weapon to the weapon's index
+            take_damage_info.weapon = weapon.index
+
+        # Is the attacker valid?
+        if attacker_index is not None:
+
+            # Set the attacker to the attacker's index
+            take_damage_info.attacker = attacker_index
 
         # Set the damage amount
         take_damage_info.damage = damage
@@ -113,12 +140,4 @@ class _EntitySpecials(object):
             setattr(take_damage_info, item, kwargs[item])
 
         # Call the function with the victim's pointer and the CTakeDamageInfo
-        victim.take_damage(take_damage_info)
-
-
-class _FakeWeapon(object):
-
-    """Class used if no weapon is provided in the damage method."""
-
-    classname = None
-    index = 0
+        self.take_damage(take_damage_info)
