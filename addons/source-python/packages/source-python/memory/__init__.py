b8b726e4d42fa9ca63acf18e49e47845e10b7aeb
Ayuto <ayuto@web.de>
2015-04-26 18:44:00 +0200

    Removed memory callback functions from the Python file

--- addons/source-python/packages/source-python/memory/__init__.py
+++ addons/source-python/packages/source-python/memory/__init__.py
@@ -16,7 +16,6 @@ from loggers import _sp_logger
 # Source.Python
 #   memory
 from _memory import BinaryFile
-from _memory import Callback
 from _memory import Convention
 from _memory import DataType
 from _memory import EXPOSED_CLASSES
@@ -36,7 +35,6 @@ from _memory import make_object
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = ('BinaryFile',
-           'Callback',
            'Convention',
            'DataType',
            'EXPOSED_CLASSES',
@@ -46,7 +44,6 @@ __all__ = ('BinaryFile',
            'StackData',
            'TYPE_SIZES',
            'alloc',
-           'callback',
            'find_binary',
            'get_object_pointer',
            'get_size',
@@ -59,27 +56,3 @@ __all__ = ('BinaryFile',
 # =============================================================================
 # Get the sp.memory logger
 memory_logger = _sp_logger.memory
-
-
-# =============================================================================
-# >> FUNCTIONS
-# =============================================================================
-def callback(convention=Convention.CDECL, args=(), return_type=DataType.VOID):
-    """Create a C++ callback that calls back to the decorated function.
-
-    EXAMPLE:
-
-    @callback(Convention.CDECL, (DataType.INT, DataType.INT), DataType.INT)
-    def add(x, y, ebp):
-        return x + y
-
-    <add> is now a Callback instance, but you can still call it like a normal
-    Python function:
-
-    assert add(4, 6) == 10
-    """
-    def wait_for_func(func):
-        """Return a memory callback instance."""
-        return Callback(func, convention, args, return_type)
-
-    return wait_for_func
