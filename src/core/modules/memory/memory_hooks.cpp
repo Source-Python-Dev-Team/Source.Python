b570a84c6f7115a74aef9ff455bed49bd9b077fa
L'In20Cible <invincible.qc@hotmail.com>
2015-05-07 13:33:20 -0400

    - Fixed issues with DataType.POINTER in Pre/Post hooks.

--- src/core/modules/memory/memory_hooks.cpp
+++ src/core/modules/memory/memory_hooks.cpp
@@ -88,8 +88,6 @@ bool SP_HookHandler(HookType_t eHookType, CHook* pHook)
 	object retval;
 	if (eHookType == HOOKTYPE_POST)
 	{
-		CPointer pReturnValue;
-
 		switch(pHook->m_pCallingConvention->m_returnType)
 		{
 			case DATA_TYPE_VOID:		retval = object(); break;
@@ -106,7 +104,7 @@ bool SP_HookHandler(HookType_t eHookType, CHook* pHook)
 			case DATA_TYPE_ULONG_LONG:	retval = GetReturnValue<unsigned long long>(pHook); break;
 			case DATA_TYPE_FLOAT:		retval = GetReturnValue<float>(pHook); break;
 			case DATA_TYPE_DOUBLE:		retval = GetReturnValue<double>(pHook); break;
-			case DATA_TYPE_POINTER:		pReturnValue = CPointer(pHook->GetReturnValue<unsigned long>()); retval = object(ptr(&pReturnValue)); break;
+			case DATA_TYPE_POINTER:		retval = object(ptr(&CPointer(pHook->GetReturnValue<unsigned long>()))); break;
 			case DATA_TYPE_STRING:		retval = GetReturnValue<const char *>(pHook); break;
 			default: BOOST_RAISE_EXCEPTION(PyExc_TypeError, "Unknown type.");
 		}
@@ -175,8 +173,6 @@ object CStackData::GetItem(unsigned int iIndex)
 	if (retval)
 		return retval;
 
-	CPointer pReturnValue;
-
 	switch(m_pHook->m_pCallingConvention->m_vecArgTypes[iIndex])
 	{
 		case DATA_TYPE_BOOL:		retval = GetArgument<bool>(m_pHook, iIndex); break;
@@ -192,7 +188,7 @@ object CStackData::GetItem(unsigned int iIndex)
 		case DATA_TYPE_ULONG_LONG:	retval = GetArgument<unsigned long long>(m_pHook, iIndex); break;
 		case DATA_TYPE_FLOAT:		retval = GetArgument<float>(m_pHook, iIndex); break;
 		case DATA_TYPE_DOUBLE:		retval = GetArgument<double>(m_pHook, iIndex); break;
-		case DATA_TYPE_POINTER:		pReturnValue = CPointer(m_pHook->GetArgument<unsigned long>(iIndex)); retval = object(ptr(&pReturnValue)); break;
+		case DATA_TYPE_POINTER:		retval = object(ptr(&CPointer(m_pHook->GetArgument<unsigned long>(iIndex)))); break;
 		case DATA_TYPE_STRING:		retval = GetArgument<const char *>(m_pHook, iIndex); break;
 		default: BOOST_RAISE_EXCEPTION(PyExc_TypeError, "Unknown type.") break;
 	}
