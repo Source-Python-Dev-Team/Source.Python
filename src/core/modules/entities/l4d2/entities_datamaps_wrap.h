b25a6eee6c30f1fe0d99ec85c299929b31b0ac0e
Stephen Toon <satoon101@gmail.com>
2015-05-26 23:54:24 -0400

    First L4D2 commit.

---
+++ src/core/modules/entities/l4d2/entities_datamaps_wrap.h
@@ -0,0 +1,54 @@
+/**
+* =============================================================================
+* Source Python
+* Copyright (C) 2014 Source Python Development Team.  All rights reserved.
+* =============================================================================
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, the Source Python Team gives you permission
+* to link the code of this program (as well as its derivative works) to
+* "Half-Life 2," the "Source Engine," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, the Source.Python
+* Development Team grants this exception to all derivative works.
+*/
+
+#ifndef _ENTITIES_DATAMAP_L4D2_WRAP_H
+#define _ENTITIES_DATAMAP_L4D2_WRAP_H
+
+//-----------------------------------------------------------------------------
+// Includes.
+//-----------------------------------------------------------------------------
+#include "datamap.h"
+
+
+//-----------------------------------------------------------------------------
+// typedescription_t extension class.
+//-----------------------------------------------------------------------------
+class TypeDescriptionExt
+{
+public:
+	static int get_offset(const typedescription_t& pTypeDesc)
+	{
+		return pTypeDesc.fieldOffset[TD_OFFSET_NORMAL];
+	}
+	
+	static int get_packed_offset(const typedescription_t& pTypeDesc)
+	{
+		return pTypeDesc.fieldOffset[TD_OFFSET_PACKED];
+	}
+};
+
+
+#endif // _ENTITIES_DATAMAP_L4D2_WRAP_H
---
+++ src/core/modules/entities/l4d2/entities_wrap.h
@@ -0,0 +1,76 @@
+/**
+* =============================================================================
+* Source Python
+* Copyright (C) 2015 Source Python Development Team.  All rights reserved.
+* =============================================================================
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, the Source Python Team gives you permission
+* to link the code of this program (as well as its derivative works) to
+* "Half-Life 2," the "Source Engine," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, the Source.Python
+* Development Team grants this exception to all derivative works.
+*/
+
+#ifndef _ENTITIES_WRAP_L4D2_H
+#define _ENTITIES_WRAP_L4D2_H
+
+//-----------------------------------------------------------------------------
+// Includes.
+//-----------------------------------------------------------------------------
+#include "dt_send.h"
+#include "game/shared/ehandle.h"
+#include "isaverestore.h"
+#include "datamap.h"
+#include "game/shared/takedamageinfo.h"
+
+
+//-----------------------------------------------------------------------------
+// CTakeDamageInfo wrapper class.
+//-----------------------------------------------------------------------------
+class TakeDamageInfoWrapper: public TakeDamageInfoBaseWrapper
+{
+public:
+	int get_penetrated()
+	{
+		return m_iPlayerPenetrationCount;
+	}
+	
+	void set_penetrated(int iPenetrated)
+	{
+		m_iPlayerPenetrationCount = iPenetrated;
+	}
+};
+
+
+//-----------------------------------------------------------------------------
+// CTakeDamageInfo extension class.
+//-----------------------------------------------------------------------------
+class TakeDamageInfoExt
+{
+public:
+	static int get_penetrated(CTakeDamageInfo *pTakeDamageInfo)
+	{
+		return ((TakeDamageInfoWrapper *)pTakeDamageInfo)->get_penetrated();
+	}
+	
+	static void set_penetrated(CTakeDamageInfo *pTakeDamageInfo, int iPenetrated)
+	{
+		((TakeDamageInfoWrapper *)pTakeDamageInfo)->set_penetrated(iPenetrated);
+	}
+};
+
+
+#endif // _ENTITIES_WRAP_L4D2_H
