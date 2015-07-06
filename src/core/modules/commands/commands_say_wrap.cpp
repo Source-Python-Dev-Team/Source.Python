5d74ea8fde3265842fbc3f95494ba1f15a55ee21
Ayuto <ayuto@web.de>
2015-07-06 23:39:51 +0200

    Updated copyright headers
    Updated plugin description

--- src/core/modules/commands/commands_say_wrap.cpp
+++ src/core/modules/commands/commands_say_wrap.cpp
@@ -1,7 +1,7 @@
 /**
 * =============================================================================
 * Source Python
-* Copyright (C) 2012 Source Python Development Team.  All rights reserved.
+* Copyright (C) 2012-2015 Source Python Development Team.  All rights reserved.
 * =============================================================================
 *
 * This program is free software; you can redistribute it and/or modify it under
