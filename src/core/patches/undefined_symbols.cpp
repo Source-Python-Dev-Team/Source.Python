66c2a9362a08b423a531b51cc849ad4520ea5aef
Ayuto <ayuto@web.de>
2015-07-13 21:10:14 +0200

    Removed an accidentally added include

--- src/core/patches/undefined_symbols.cpp
+++ src/core/patches/undefined_symbols.cpp
@@ -33,7 +33,6 @@
 #include "game/server/entitylist.h"
 #include "utilities/conversions.h"
 #include "game/shared/ipredictionsystem.h"
-#include "memalloc.h"
 
 #include ENGINE_INCLUDE_PATH(undefined_symbols.h)
 
