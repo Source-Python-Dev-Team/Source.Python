8d5eacb633b81912aa548a1a026541ae3e6c689f
Ayuto <ayuto@web.de>
2015-09-06 21:11:23 +0200

    Added a class to easily create user messages
    Implemented VGUIMenu

--- addons/source-python/packages/source-python/messages/__init__.py
+++ addons/source-python/packages/source-python/messages/__init__.py
@@ -8,6 +8,7 @@
 # Source.Python Imports
 #   Loggers
 from loggers import _sp_logger
+from filters.recipients import RecipientFilter
 
 
 # ============================================================================
@@ -15,14 +16,16 @@ from loggers import _sp_logger
 # ============================================================================
 #   Messages
 from _messages import DialogType
-from _messages import send_saytext2
+from _messages import UserMessage
 
 
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = ('DialogType',
-           'send_saytext2',
+           'UserMessage',
+           'UserMessageCreator',
+           'VGUIMenu',
            )
 
 
@@ -34,3 +37,82 @@ messages_logger = _sp_logger.messages
 
 # TODO: Temp "fix" to be able to load the plugin
 ShowMenu = None
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class UserMessageCreator(object):
+
+    """Provide an easy interface to create user messages."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the user message."""
+        if kwargs:
+            raise ValueError('Do not provide keywords.')
+
+        self.args = args
+
+    def send(self, *player_indexes):
+        """Send the user message."""
+        # TODO: Handle translations
+        user_message = UserMessage(
+            RecipientFilter(*player_indexes), self.message_name)
+
+        if user_message.is_protobuf:
+            self.protobuf(user_message.buffer, *self.args)
+        else:
+            self.bitbuf(user_message.buffer, *self.args)
+
+        user_message.send()
+
+    def protobuf(self, buffer, *args):
+        """Protobuf implementation of this user message."""
+        raise NotImplementedError('Must be implemented by a subclass.')
+
+    def bitbuf(self, buffer, *args):
+        """Bitbuf implementation of this user message."""
+        raise NotImplementedError('Must be implemented by a subclass.')
+
+    @property
+    def message_name(self):
+        """Return the user message name."""
+        raise NotImplementedError('Must be implemented by a subclass.')
+
+
+class VGUIMenu(UserMessageCreator):
+
+    """Create a VGUIMenu."""
+
+    message_name = 'VGUIMenu'
+
+    def __init__(self, name, subkeys={}, show=True):
+        """Initialize the VGUI menu.
+
+        @param <name>:
+        A string that defines the name of the menu.
+
+        @param <show>:
+        If True the menu will be shown, else it will be hidden.
+
+        @param <subkeys>:
+        A dictionary that defines the data for the menu.
+        """
+        # TODO: Which names and subkeys are available?
+        super().__init__(name, subkeys, show)
+
+    def protobuf(self, buffer, name, subkeys, show):
+        buffer.set_string('name', name)
+        buffer.set_bool('show', show)
+        for key, value in subkeys.items():
+            temp_buffer = buffer.add_message('subkeys')
+            temp_buffer.set_string('name', key)
+            temp_buffer.set_string('str', value)
+
+    def bitbuf(self, buffer, name, subkeys, show):
+        buffer.write_string(name)
+        buffer.write_byte(show)
+        buffer.write_byte(len(subkeys))
+        for key, value in subkeys.items():
+            buffer.write_string(key)
+            buffer.write_string(value)
