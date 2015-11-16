edf70c491b41593ea6ba525b3c19b057808eb007
Stephen Toon <satoon101@gmail.com>
2015-11-16 13:45:45 -0500

    Added documentation for cvars.tags.

--- addons/source-python/packages/source-python/cvars/tags.py
+++ addons/source-python/packages/source-python/cvars/tags.py
@@ -24,13 +24,19 @@ class _SVTags(ConVar):
     """Class used to interact wit the sv_tags ConVar."""
 
     def add(self, tag):
-        """Add the given tag to sv_tags."""
+        """Add the given tag to sv_tags.
+
+        :param str tag: The tag to add to the sv_tags ConVar value.
+        """
         tags = set(self.get_string().split(','))
         tags.add(tag)
         self.set_string(','.join(tags))
 
     def remove(self, tag):
-        """Remove the given tag from sv_tags."""
+        """Remove the given tag from sv_tags.
+
+        :param str tag: The tag to remove from the sv_tags ConVar value.
+        """
         tags = set(self.get_string().split(','))
         tags.discard(tag)
         self.set_string(','.join(tags))
