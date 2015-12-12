b8af642484c78c02b4f9ca90026cf2405f66fca2
L'In20Cible <invincible.qc@hotmail.com>
2015-12-11 23:59:00 -0500

    Exported blood color constants as effects.constants.BloodColor enumerator.

--- addons/source-python/packages/source-python/effects/constants.py
+++ addons/source-python/packages/source-python/effects/constants.py
@@ -1,5 +1,25 @@
 # ../effects/constants.py
 
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Python Imports
+#   Enum
+from enum import IntEnum
+
+
+# =============================================================================
+# >> FORWARD IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Effects
+from _effects._constants import DONT_BLEED
+from _effects._constants import BLOOD_COLOR_RED
+from _effects._constants import BLOOD_COLOR_YELLOW
+from _effects._constants import BLOOD_COLOR_GREEN
+from _effects._constants import BLOOD_COLOR_MECH
+
+
 # ============================================================================
 # >> ALL DECLARATION
 # ============================================================================
@@ -23,4 +43,15 @@ ALIAS_ALPHA_NAME = 'alpha'
 # Alias used by the "model" property...
 ALIAS_MODEL_INDEX_NAME = "model_index"
 
-ALIAS_BEAM_HALO_INDEX_NAME = 'halo_index'
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+class BloodColor(IntEnum):
+    """Blood colors wrapper enumerator."""
+
+    DONT_BLEED = DONT_BLEED
+    RED = BLOOD_COLOR_RED
+    YELLOW = BLOOD_COLOR_YELLOW
+    GREEN = BLOOD_COLOR_GREEN
+    MECH = BLOOD_COLOR_MECH
