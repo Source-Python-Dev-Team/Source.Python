bb5c7e345c1ff3b87a5a281064e6c31b3cac34d1
Stephen Toon <satoon101@gmail.com>
2015-12-26 13:50:48 -0500

    Updated base get_client_language to not attempt to get the cl_language value for bots.

--- addons/source-python/packages/source-python/players/_language/__init__.py
+++ addons/source-python/packages/source-python/players/_language/__init__.py
@@ -15,4 +15,9 @@ from engines.server import engine_server
 # =============================================================================
 def get_client_language(index):
     """Return the language of the given client."""
+    from players.helpers import playerinfo_from_index
+    playerinfo = playerinfo_from_index(index)
+    if (playerinfo.is_fake_client() or
+            'BOT' in playerinfo.get_networkid_string()):
+        return ''
     return engine_server.get_client_convar_value(index, 'cl_language')
