20e397b0c46763f8b0421791808545824eafa25c
Ayuto22 <ayuto@web.de>
2016-02-02 21:14:26 +0100

    Renamed BM:S specific methods

--- src/core/modules/engines/bms/engines_wrap.h
+++ src/core/modules/engines/bms/engines_wrap.h
@@ -48,18 +48,20 @@ template<class T>
 T IVEngineServer_Visitor(T cls)
 {
 	cls
-		.def("get_game_server_steamid",
-			&IVEngineServer::GetGameServerSteamID,
-			"Returns the SteamID of the game server.",
-			reference_existing_object_policy()
+		.add_property("game_server_steamid",
+			make_function(
+				&IVEngineServer::GetGameServerSteamID,
+				reference_existing_object_policy()
+			),
+			"Returns the SteamID of the game server."
 		)
 
-		.def("get_server_version",
+		.add_property("server_version",
 			&IVEngineServer::GetServerVersion,
 			"Returns the version of the server."
 		)
 
-		.def("get_time",
+		.add_property("time",
 			&IVEngineServer::Time,
 			"Returns a high precision timer for doing profiling work"
 		)
@@ -74,9 +76,11 @@ T IVEngineServer_Visitor(T cls)
 			reference_existing_object_policy()
 		)
 
-		.def("get_server",
-			&IVEngineServer::GetIServer,
-			reference_existing_object_policy()
+		.add_property("server",
+			make_function(
+				&IVEngineServer::GetIServer,
+				reference_existing_object_policy()
+			)
 		)
 	;
 
