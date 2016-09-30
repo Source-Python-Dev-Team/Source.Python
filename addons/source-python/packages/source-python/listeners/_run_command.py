9c444775739566f37667e12dee482aa2e8d23a0d
Ayuto22 <ayuto@web.de>
2016-09-30 17:12:33 +0200

    Fixed issue #149

--- addons/source-python/packages/source-python/listeners/_run_command.py
+++ addons/source-python/packages/source-python/listeners/_run_command.py
@@ -6,6 +6,8 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python Imports
+#   Core
+from core import SOURCE_ENGINE_BRANCH
 #   Entities
 from entities.hooks import EntityPreHook
 from entities.hooks import EntityCondition
@@ -17,6 +19,14 @@ from players import UserCmd
 from players.entity import Player
 #   Memory
 from memory import make_object
+from memory import get_object_pointer
+from memory import get_size
+
+
+# =============================================================================
+# >> CONSTANTS
+# =============================================================================
+USER_CMD_SIZE = get_size(UserCmd)
 
 
 # =============================================================================
@@ -29,13 +39,24 @@ def _pre_player_run_command(args):
         return
 
     player = make_object(Player, args[0])
-    user_cmd = make_object(UserCmd, args[1])
+
+    # https://github.com/Source-Python-Dev-Team/Source.Python/issues/149
+    use_rtti_fix = SOURCE_ENGINE_BRANCH == 'tf2' and player.is_fake_client()
+    if use_rtti_fix:
+        user_cmd = UserCmd()
+        user_cmd_ptr = get_object_pointer(user_cmd)
+        args[1].copy(user_cmd_ptr, USER_CMD_SIZE)
+    else:
+        user_cmd = make_object(UserCmd, args[1])
 
     if on_player_run_command_listener_manager:
         on_player_run_command_listener_manager.notify(player, user_cmd)
 
     if on_button_state_changed_listener_manager:
         _handle_button_state_changed(player, player.buttons, user_cmd.buttons)
+
+    if use_rtti_fix:
+        user_cmd_ptr.copy(args[1], USER_CMD_SIZE)
 
 
 def _handle_button_state_changed(player, old_buttons, new_buttons):
@@ -44,4 +65,3 @@ def _handle_button_state_changed(player, old_buttons, new_buttons):
 
     on_button_state_changed_listener_manager.notify(
         player, old_buttons, new_buttons)
-
