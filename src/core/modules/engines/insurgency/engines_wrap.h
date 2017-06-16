083c904d429ad9583d3483426feacfcb0d3d7a5b
Ayuto22 <ayuto@web.de>
2017-06-16 20:46:27 +0200

    Added missing engine specific export function

--- src/core/modules/engines/insurgency/engines_wrap.h
+++ src/core/modules/engines/insurgency/engines_wrap.h
@@ -96,4 +96,20 @@ T IEngineSound_Visitor(T cls)
 	return cls;
 }
 
+
+//-----------------------------------------------------------------------------
+// Exports IServerGameDLL.
+//-----------------------------------------------------------------------------
+template<class T, class U>
+void export_engine_specific_server_game_dll(T _server, U ServerGameDLL)
+{
+	// Methods...
+	ServerGameDLL.def("server_hibernation_update", &IServerGameDLL::ServerHibernationUpdate);
+
+	// Class info...
+	BEGIN_CLASS_INFO(IServerGameDLL)
+		FUNCTION_INFO(ServerHibernationUpdate)
+	END_CLASS_INFO()
+}
+
 #endif // _ENGINES_WRAP_INSURGENCY_H
