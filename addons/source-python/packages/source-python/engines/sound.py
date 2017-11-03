1a01db3a57acb7588b1836e5f6c3835a135f640c
jayess <jayess@jayess.za.net>
2017-11-03 15:53:09 +0200

    Change Sound.is_precached and StreamSound.is_precached to not iterate over string_tables.soundprecache.

--- addons/source-python/packages/source-python/engines/sound.py
+++ addons/source-python/packages/source-python/engines/sound.py
@@ -34,7 +34,7 @@ from mathlib import NULL_VECTOR
 #   Paths
 from paths import GAME_PATH
 #   Stringtables
-from stringtables import string_tables
+from stringtables import INVALID_STRING_INDEX, string_tables
 from stringtables.downloads import Downloadables
 
 
@@ -323,7 +323,7 @@ class Sound(_BaseSound):
 
         :rtype: bool
         """
-        return self.sample in string_tables.soundprecache
+        return string_tables.soundprecache[self.sample] != INVALID_STRING_INDEX
 
 
 class StreamSound(_BaseSound):
@@ -368,4 +368,5 @@ class StreamSound(_BaseSound):
 
         :rtype: bool
         """
-        return self._stream_sample in string_tables.soundprecache
+        index = string_tables.soundprecache[self._stream_sample]
+        return index != INVALID_STRING_INDEX
