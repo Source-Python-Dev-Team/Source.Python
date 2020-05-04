b8afdb6b14a395484c967d3c9d7088debad68817
Jordan Brière <github@jordanbriere.com>
2020-05-04 03:34:01 -0400

    Optimized non-cached get_client_language by using the internal Player cache instead of PlayerInfo.

--- addons/source-python/packages/source-python/players/_language/base.py
+++ addons/source-python/packages/source-python/players/_language/base.py
@@ -18,9 +18,8 @@ def get_client_language(index):
 
     :param int index: Index of the client.
     """
-    from players.helpers import playerinfo_from_index
-    playerinfo = playerinfo_from_index(index)
-    if playerinfo.is_fake_client() or 'BOT' in playerinfo.steamid:
+    from players.entity import Player
+    if Player(index).is_bot():
         return ''
 
     return engine_server.get_client_convar_value(index, 'cl_language')
