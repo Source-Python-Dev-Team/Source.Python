759953f727fac97261e0c9822b2c36fa14a81ea4
Jonathan <30329245+CookStar@users.noreply.github.com>
2021-10-02 21:17:21 +0900

    Added ConVarChanged decorator class to cvars.

--- addons/source-python/packages/source-python/cvars/__init__.py
+++ addons/source-python/packages/source-python/cvars/__init__.py
@@ -6,6 +6,8 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python Imports
+#   Core
+from core import AutoUnload
 #   Cvars
 from _cvars import ConVar
 from _cvars import _Cvar
@@ -24,5 +26,64 @@ from _cvars import cvar
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = ('ConVar',
+           'ConVarChanged',
            'cvar',
            )
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class ConVarChanged(AutoUnload):
+    """ConVarChanged decorator class."""
+
+    def __init__(self, *convars):
+        """Store the convars."""
+        self._convars = ()
+        self.callback = None
+
+        # Validate convars
+        if not convars:
+            raise ValueError('At least one convar is required.')
+
+        _convars = []
+        for convar in convars:
+            if not isinstance(convar, (str, ConVar)):
+                raise ValueError('Given convar is not ConVar or ConVar name.')
+
+            elif isinstance(convar, str):
+                convar_name = convar
+                convar = cvar.find_var(convar_name)
+                if convar is None:
+                    raise ValueError(
+                        f'"{convar_name}" is not a valid ConVar name.')
+
+            _convars.append(convar)
+
+        self._convars = tuple(_convars)
+
+    def __call__(self, callback):
+        """Store the callback and add it to convars."""
+        # Store the callback
+        self.callback = callback
+
+        # Loop through all convars
+        for convar in self._convars:
+
+            # Add the callback
+            convar.add_changed_callback(self.callback)
+
+        # Return the callback
+        return self.callback
+
+    def _unload_instance(self):
+        """Remove the callback from convars."""
+        # Was no callback registered?
+        if self.callback is None:
+            return
+
+        # Loop through all convars
+        for convar in self._convars:
+
+            # Remove the callback
+            convar.remove_changed_callback(self.callback)
