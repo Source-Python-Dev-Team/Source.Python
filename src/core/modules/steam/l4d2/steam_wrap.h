d0ffd8ccbd1e9923c9bc44936f20613c1c76b7fb
Jordan Brière <github@jordanbriere.com>
2021-12-13 20:32:05 -0500

    Removed Universe.RC.

--- src/core/modules/steam/l4d2/steam_wrap.h
+++ src/core/modules/steam/l4d2/steam_wrap.h
@@ -33,12 +33,6 @@ void export_engine_specific_steamid(scope _steam, T SteamID)
 }
 
 template<class T>
-void export_engine_specific_universe(scope _steam, T Universe)
-{
-	Universe.value("RC", k_EUniverseRC);
-}
-
-template<class T>
 void export_engine_specific_account_type(scope _steam, T AccountType)
 {
 	AccountType.value("P2P_SUPER_SEEDER", k_EAccountTypeP2PSuperSeeder);
